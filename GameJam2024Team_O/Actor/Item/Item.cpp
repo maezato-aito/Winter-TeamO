f048b7623891d63c07ea278b141083afc5d8466c
masahiko-1008 <138412620+masahiko-1008@users.noreply.github.com>
2024-02-13 11:42:34 +0900

    制作

--- GameJam2024Team_O/Actor/Item/Item.cpp
+++ GameJam2024Team_O/Actor/Item/Item.cpp
@@ -1,9 +1,9 @@
 #include "Item.h"
 #include"DxLib.h"
-
+#include"../../Scene/GameMain/GameMainScene.h"
 Item::Item()
 {
-	
+	Initialize();
 }
 
 Item::~Item()
@@ -11,16 +11,47 @@ Item::~Item()
 
 }
 
-
-
-void Item::Update()
+//初期化処理
+void Item::Initialize()
 {
+	//出現させるX座標パターンを取得
+	float random_x = (float)(GetRand(4) * 105 + 40);
+	//生成位置の設定
+	location.x= random_x;
+	location.y=0;
+	//当たり判定の 
+	area.width = 100;
+	area.height = 100;
+	//速さの設定
+	speed = 1;
+}
+
+void Item::Update(GameMainScene*game)
+{
+	//位置情報に移動量を加算する
+	location.y += speed;
+	if (HitBox(game->GetPlayer1()))
+	{
+		delete this;
+	}
 	
 }
 
 void Item::Draw()
 {
-	
+	//画像の描画
+	DrawBox(location.x,location.y, location.x+area.width,location.y+area.height,0xffffff,TRUE);
+}
+
+void Item::Finalize()
+{
+
+}
+
+//タイプを取得
+int Item::GetType() const
+{
+	return type;
 }
 
 
--- GameJam2024Team_O/Actor/Item/Item.h
+++ GameJam2024Team_O/Actor/Item/Item.h
@@ -1,13 +1,22 @@
 #pragma once
 #include"../../Collision/BoxCollision.h"
+class GameMainScene;
 class Item:public BoxCollision 
 {
+private:
+	int type;        //タイプ
+	int image;       //画像
+	float speed;     //速さ
 
 public:
 	Item();
 	~Item();
 
-	void Update();
-	void Draw();
+	void Initialize();       //初期化処理
+	void Update(GameMainScene*);           //更新処理
+	void Draw();             //描画処理
+	void Finalize();         //終了時処理
+
+	int GetType() const;     //タイプ取得
 };
 
