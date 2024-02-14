cb5b81fae0af8f5865cb6f27a3d1d42fd9175a27
nigirimeshi02 <104492636+nigirimeshi02@users.noreply.github.com>
2024-02-14 10:35:06 +0900

    各シーンの骨組み追加

---
+++ GameJam2024Team_O/Scene/End/EndScene.cpp
@@ -0,0 +1,21 @@
+#include "EndScene.h"
+
+EndScene::EndScene()
+{
+
+}
+
+EndScene::~EndScene()
+{
+
+}
+
+SceneBase* EndScene::Update()
+{
+	return this;
+}
+
+void EndScene::Draw() const
+{
+
+}
---
+++ GameJam2024Team_O/Scene/Title/TitleScene.cpp
@@ -0,0 +1,21 @@
+#include "TitleScene.h"
+
+TitleScene::TitleScene()
+{
+
+}
+
+TitleScene::~TitleScene()
+{
+
+}
+
+SceneBase* TitleScene::Update()
+{
+	return this;
+}
+
+void TitleScene::Draw() const
+{
+
+}
